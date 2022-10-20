0f4f9091dddec1292473734466a3999f99c73cc9
berkaypacal <pacalberkay@gmail.com>
2022-10-20 10:17:28 +0300

    add chat view and viewmodel

--- lib/src/view-model/chat/chat_view_model.dart
+++ lib/src/view-model/chat/chat_view_model.dart
@@ -1,5 +1,63 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:signalr_mobile/src/service/signalr/hubconnection_helper.dart';
+import 'package:signalr_mobile/src/service/signalr/signalr_service.dart';
+
+import '../../model/message.dart';
 import '../../view/chat/chat_view.dart';
 
-abstract class ChatViewModel extends State<ChatView> {}
+abstract class ChatViewModel extends State<ChatView> {
+  final String appBarTitle = "Home";
+  final String connectionText = "Connect to chat room";
+  final TextEditingController messageController = TextEditingController();
+  late final SignalRService signalRService;
+  List<Message> messageList = [];
+  String connectionId = "";
+
+  @override
+  void initState() {
+    super.initState();
+    signalRService = SignalRService(HubConnectionHelper.instance.hubConnection);
+    _startConnection().then((value) {
+      _getConnectionId();
+      _listenBroadcast();
+    }).catchError((error) {
+      if (kDebugMode) {
+        print(error);
+      }
+    });
+  }
+
+  Future<void> _getConnectionId() async {
+    connectionId = await signalRService.getConnectionId();
+    setState(() {});
+  }
+
+  Future<void> _startConnection() async {
+    await signalRService.startConnection();
+  }
+
+  Future<void> _listenBroadcast() async {
+    signalRService.hubConnection.on("BroadcastMessage", (result) {
+      if (result!.isNotEmpty) {
+        for (var element in result) {
+          Message message = Message.fromJson(element as Map<String, dynamic>);
+          messageList.add(message);
+          setState(() {});
+          inspect(message);
+        }
+      }
+    });
+  }
+
+  Future<void> sendMessage() async {
+    Message message = Message(
+      connectionId: connectionId,
+      message: messageController.text,
+    );
+    await signalRService.sendMessage(message);
+    messageController.clear();
+  }
+}
--- lib/src/view/chat/chat_view.dart
+++ lib/src/view/chat/chat_view.dart
@@ -1,4 +1,6 @@
+import 'package:chat_bubbles/bubbles/bubble_special_three.dart';
 import 'package:flutter/material.dart';
+import 'package:signalr_mobile/src/model/message.dart';
 import 'package:signalr_mobile/src/view-model/chat/chat_view_model.dart';
 
 class ChatView extends StatefulWidget {
@@ -12,7 +14,90 @@ class _ChatViewState extends ChatViewModel {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(appBarTitle),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: messageList.length,
+              itemBuilder: (BuildContext context, int index) {
+                String messageConnectionId = messageList[index].connectionId ?? "";
+                return _ChatBubbleComponent(
+                  model: messageList[index],
+                  connectionId: connectionId,
+                  messageConnectionId: messageConnectionId,
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            child: _ChatTextFieldComponent(
+              messageController: messageController,
+              onPressed: () {
+                sendMessage();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ChatBubbleComponent extends StatelessWidget {
+  const _ChatBubbleComponent({
+    Key? key,
+    required this.model,
+    required this.connectionId,
+    required this.messageConnectionId,
+  }) : super(key: key);
+
+  final Message model;
+  final String connectionId;
+  final String messageConnectionId;
+
+  @override
+  Widget build(BuildContext context) {
+    return BubbleSpecialThree(
+      text: model.message ?? "",
+      color: const Color(0xFF1B97F3),
+      tail: true,
+      isSender: connectionId == messageConnectionId ? true : false,
+      textStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Colors.white),
+    );
+  }
+}
+
+class _ChatTextFieldComponent extends StatelessWidget {
+  const _ChatTextFieldComponent({
+    Key? key,
+    required this.messageController,
+    required this.onPressed,
+  }) : super(key: key);
+  final void Function() onPressed;
+  final TextEditingController messageController;
+  final String placeholder = "Send message";
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: TextField(
+            controller: messageController,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              labelText: placeholder,
+            ),
+          ),
+        ),
+        IconButton(onPressed: () => onPressed.call(), icon: const Icon(Icons.send))
+      ],
     );
   }
 }
