d6d4b30ab6fc2a98c2c8aa3b9344ee678ac36b80
berkaypacal <pacalberkay@gmail.com>
2022-10-21 01:28:30 +0300

    add enum for function names

--- lib/src/service/signalr/signalr_service.dart
+++ lib/src/service/signalr/signalr_service.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:signalr_mobile/src/service/signalr/signalr_function_enum.dart';
 import 'package:signalr_netcore/signalr_client.dart';
 
 import '../../model/message.dart';
@@ -9,33 +9,21 @@ class SignalRService {
   SignalRService(this.hubConnection);
 
   Future<void> startConnection() async {
-    await hubConnection.start()?.catchError((error) {
-      if (kDebugMode) {
-        print("Connection failed $error");
-      }
-    });
+    await hubConnection.start()?.catchError((error) {});
 
-    hubConnection.onclose(({error}) {
-      if (kDebugMode) {
-        print("Hub connection stopped automatically");
-        if (error != null) print("Error $error");
-      }
-    });
+    hubConnection.onclose(({error}) {});
   }
 
   Future<void> stopConnection() async {
     await hubConnection.stop();
-    if (kDebugMode) {
-      print("Hub connection stopped");
-    }
   }
 
   Future<void> sendMessage(Message message) async {
-    await hubConnection.invoke("SendMessage", args: <Message>[message]);
+    await hubConnection.invoke(SignalrFunctionEnum.SendMessage.name, args: <Message>[message]);
   }
 
   Future<String> getConnectionId() async {
-    var connectionIdResponse = await hubConnection.invoke("GetConnectionId");
+    var connectionIdResponse = await hubConnection.invoke(SignalrFunctionEnum.GetConnectionId.name);
     if (connectionIdResponse != null) {
       return connectionIdResponse.toString();
     }
